4e92b167c600df5e89bbc117d61714653e830c3e
Vethursan Jegatheeswaran <j.vethursan@gmail.com>
2018-12-16 13:33:58 -0500

    put back notes on reducers

--- reducers/todos.js
+++ reducers/todos.js
@@ -1,3 +1,17 @@
+/*
+Reducers:
+
+Reducers specify how your applications state changes in response to the actions sent to the store
+Note: Actions describe what happened but not how the state changes <- reducers do that.
+
+Pure functions that take in the previous state and action and returns the next state.
+(prevState, action) => newState
+
+Inside a reducer NEVER:
+    -mutate its arguments
+    -perfrom side effects like API calls and route transitions
+    -call non pure funcs like Date.now() nd math.random() 
+*/
 const todos = (state = [], action) => {
   switch (action.type) {
     case "ADD_TODO":
